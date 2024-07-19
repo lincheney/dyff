eca06c1b24230b3c55fbf3c21527c5356623242a
林千里 <lincheney@gmail.com>
2024-07-20 02:10:18 +1000

    basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use clap::Parser;
+
+#[derive(Clone, Debug, clap::ValueEnum)]
+enum ColorChoices {
+    Never,
+    Auto,
+    Always,
+}
+
+#[derive(Debug, clap::Parser)]
+#[command(name = "diff")]
+struct Cli {
+
+    #[arg(long, value_enum, default_value_t = ColorChoices::Always)]
+    color: ColorChoices,
+
+    #[arg(short = 'N', long = "no-line-numbers", action = clap::ArgAction::SetFalse)]
+    line_numbers: bool,
+
+    #[arg(short, long)]
+    signs: bool,
+
+    #[arg(long)]
+    exact: bool,
+
+    #[arg(short, long)]
+    filter: Option<String>,
+
+    /// use LABEL instead of file name and timestamp (can be repeated)
+    #[arg(long)]
+    label: Vec<String>,
+
+    file1: Option<String>,
+    file2: Option<String>,
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    eprintln!("DEBUG(marry) \t{}\t= {:?}", stringify!(cli), cli);
 }
