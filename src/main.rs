4a6afed4f092231819c0bed7b4d963fd88ae2892
林千里 <lincheney@gmail.com>
2024-11-04 05:06:50 +1100

    diff --git should have a/ b/ prefixes

--- src/main.rs
+++ src/main.rs
@@ -248,7 +248,13 @@ fn _main() -> Result<ExitCode> {
             continue
         }
 
-        if let Some(captures) = regex!("^(?<header>diff( -r| --recursive| --git| --cc)?) (?<filename1>[^\"\\s-][^\"\\s]+|\"(\\\\.|.)*\") (?<filename2>[^\"\\s]+|\"(\\\\.|.)*\")(?<trailer>.*)".captures(&stripped)) {
+
+        if let Some(captures) =
+            regex!("^(?<header>diff( -r| --recursive)?) (?<filename1>[^\"\\s-][^\"\\s]+|\"(\\\\.|.)*\") (?<filename2>[^\"\\s]+|\"(\\\\.|.)*\")(?<trailer>.*)".captures(&stripped))
+            .or_else(||
+                regex!("^(?<header>diff( --git| --cc)) (?<filename1>a/.*) (?<filename2>b/.*)(?<trailer>.*)".captures(&stripped))
+            )
+        {
             if let Some(mut hunk) = hunk {
                 hunk.print(&mut stdout, line_numbers, merge_markers.as_ref(), style)?;
             }
